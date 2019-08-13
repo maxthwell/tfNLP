54dbaf203429962a5cbda3cd3f2b6bee517a19f8
maxthwell <1037959324@qq.com>
2019-08-13 06:42:43 -0400

    添加命名实体识别模块

---
+++ data_processor/ner_processor.py
@@ -0,0 +1,108 @@
+import os, sys, random
+import jieba
+
+class BaseDataProcessor():
+    def __init__(self):
+        pass
+    
+    def batch_sample(batch_size=100, **kwargs):
+        pass
+
+
+class NerDataProcessor(BaseDataProcessor):
+    def __init__(self, train_data_dir, test_data_dir, cv_data_dir, num_step=10, dict_path=None):
+        #训练集目录
+        self.train_data_dir=train_data_dir
+        #测试集目录
+        self.test_data_dir=test_data_dir
+        #交叉验证集目录
+        self.cv_data_dir=cv_data_dir
+        self.dict_path=dict_path
+        assert(os.listdir(self.train_data_dir), os.listdir(self.test_data_dir))
+        assert(os.listdir(self.train_data_dir), os.listdir(self.cv_data_dir))
+        self.label_list = os.listdir(self.test_data_dir)
+        self.label_dict = {label:idx for idx,label in enumerate(self.label_list)}
+        self.num_label = len(self.label_list)
+        self.num_step=num_step
+        self.word_dict = self.load_dict()
+        self.num_words = len(self.word_dict)
+    
+    def load_dict(self):
+        def load(fp):
+            wd = {}
+            idx=1
+            for line in fp:
+                w=str(line,'utf8').split()[0]
+                print(w)
+                wd[w]=idx
+                idx += 1
+            return wd
+
+        if self.dict_path:
+            with open(self.dict_path, 'r') as fp:
+                return load(fp)
+        with jieba.get_dict_file() as fp:
+            return load(fp)
+      
+
+    def batch_sample(self, batch_size=100, **kwargs):
+        wt = kwargs['work_type'] if 'work_type' in kwargs else 'train'
+        if wt=='train':
+            data_dir=self.train_data_dir
+        elif wt=='test':
+            data_dir=self.test_data_dir
+        elif wt=='cv':
+            data_dir=self.cv_data_dir
+        is_random = (wt != 'test')
+        all_files = []
+        for label in self.label_list:
+            for fn in os.listdir('%s/%s'%(data_dir,label)):
+                all_files.append((data_dir,label,fn))
+        len_samples = len(all_files)
+        i=0
+        while True:
+            if is_random:
+                sample_file_list = random.sample(all_files,batch_size)
+            else:
+                sample_file_list = all_files[i:i+batch_size]
+                i += batch_size
+            N=[]
+            S=[]
+            X=[]
+            Y=[]
+            for fs in sample_file_list:
+                N.append('%s/%s/%s'%fs)
+                s,x = self.process_file(fs)
+                S.append(s)
+                X.append(x)
+                Y.append(self.label_dict[fs[1]])
+            yield N,S,X,Y
+
+    def process_file(self, fs):
+        with open('%s/%s/%s'%fs, 'r') as fp: s = fp.read()
+        #先进行分词  
+        words = list(jieba.cut(s, cut_all=True)) 
+        #初始化向量序列  
+        data = [0 for i in range(self.num_step)] 
+        j = 0  
+        #按照词序，依次把用词向量填充序列  
+        for i in range(len(words)):
+            if i == self.num_step:  
+                break  
+            w = words[i]  
+            if w in self.word_dict:  
+                data[i] = self.word_dict[w]  
+        return i, data 
+
+if __name__=='__main__':
+    cdp = ClassiffierDataProcessor(
+      train_data_dir='/data/THUCNews',
+      test_data_dir='/data/THUCNewsTest',
+      cv_data_dir='/data/THUCNewsTest',
+    )
+    for N,S,X,Y in cdp.batch_sample(2):
+        print(N)
+        print(S)
+        print(X)
+        print(Y)
+        sys.exit(0)
--- ner/brnn_crf.py
+++ ner/brnn_crf.py
@@ -3,21 +3,20 @@ import random
 import numpy as np
 import scipy as sp
 import tensorflow as tf
-from modeling.base_model import WordEmbedding, BiRnn, Classiffier, Attention
-from data_processor.clf_processor import ClassiffierDataProcessor as CDP
-from classiffier.evaluator import ClassiffierModelEvaluator
+from modeling.base_model import WordEmbedding, BiRnn, CRF
+from data_processor.ner_processor import NerDataProcessor as NDP
 
-class OnlyAttentionClassiffier():
-    def __init__(self, num_step, num_words, num_label, model_path=None):
+class BrnnCrfNer():
+    def __init__(self, num_step, num_words, num_tags, model_path=None):
         self.model_path=model_path
         self.num_step=num_step
-        self.num_label=num_label
+        self.num_tags=num_tags
         self.num_words=num_words
         with tf.variable_scope('only_attention_classiffier'):
             self.we = WordEmbedding(num_step=num_step,dict_size=num_words,word_vec_size=50)
-            attention = Attention(inputs=self.we.outputs)
-            self.clf = Classiffier(inputs=attention.outputs, num_label=num_label)
-            self.train_op = tf.train.AdamOptimizer(1e-3).minimize(self.clf.loss, global_step=self.clf.global_step)
+            self.brnn = BiRnn(inputs=self.we.outputs, rnn_size_list=[30], rnn_type='gru')
+            self.crf = CRF(inputs=self.brnn.outputs,sequence_length=self.brnn.sequence_length)
+            self.train_op = tf.train.AdamOptimizer(1e-3).minimize(self.crf.loss, global_step=self.crf.global_step)
             self.saver = tf.train.Saver()
             for var in tf.trainable_variables(): print(var.name)   
 
@@ -35,18 +34,15 @@ class OnlyAttentionClassiffier():
         S,L,X,Y = next(cv_generator)
         fd={                                                                 
           self.we.inputs: np.array(X,dtype=np.int32),                        
-          self.clf.exp_label: np.array(Y,dtype=np.int32),
+          self.brnn.sequence_length: np.array(L),
+          self.crf.exp_tags: np.array(Y,dtype=np.int32),
         }
         t0=time.time()
-        step,loss,acc,all_label = sess.run([self.clf.global_step, self.clf.loss, self.clf.acc, self.clf.all_label],feed_dict=fd)
+        step,loss,acc= sess.run([self.crf.global_step, self.crf.loss, self.crf.acc],feed_dict=fd)
         t1=time.time()
         print('----------------- global_step: ',step)
         print('----------------- predict 10000 samples use time: %s sencond'%(t1-t0))
-        eva = ClassiffierModelEvaluator(num_label=self.num_label,label_name_list=label_list)
-        eva.batch_add(all_label)
-        eva.update_quota()
         print('cross-validation ---------- loss: %s, acc:%s'%(loss,acc))
-        print(eva)
            
 
     def train(self,sess=None,train_generator=None,cv_generator=None,epochs=100):
@@ -56,9 +52,10 @@ class OnlyAttentionClassiffier():
                 S,L,X,Y = next(train_generator)
                 fd={
                   self.we.inputs: np.array(X,dtype=np.int32),
-                  self.clf.exp_label: np.array(Y,dtype=np.int32),
+                  self.brnn.sequence_length: np.array(L),
+                  self.crf.exp_tags: np.array(Y,dtype=np.int32),
                 }
-                _,step,loss,acc = sess.run([self.train_op, self.clf.global_step, self.clf.loss, self.clf.acc],feed_dict=fd)
+                _,step,loss,acc = sess.run([self.train_op, self.crf.global_step, self.crf.loss, self.crf.acc],feed_dict=fd)
                 print('step: %s, loss: %s, acc:%s'%(step,loss,acc))
             print('save model: ',self.model_path)
             self.saver.save(sess, self.model_path)
@@ -75,13 +72,15 @@ class OnlyAttentionClassiffier():
 
 
 if __name__=='__main__':
-    cdp = CDP(
+    '''
+    ndp = NDP(
       train_data_dir='/data/THUCNews',
       test_data_dir='/data/THUCNewsTest',
       cv_data_dir='/data/THUCNewsTest',
       num_step = 1000,
     )
-    m=OnlyAttentionClassiffier(num_label=cdp.num_label,num_step=cdp.num_step,num_words=cdp.num_words, model_path='/root/tfNLP/motc/clf/only_attention/model')
-    train_generator = cdp.batch_sample(batch_size=1000)
-    cv_generator = cdp.batch_sample(batch_size=10000,work_type='cv')
+    m=BrnnAttentionClassiffier(num_label=ndp.num_label,num_step=ndp.num_step,num_words=ndp.num_words, model_path='/root/tfNLP/motc/ner/brnn_crf/model')
+    train_generator = ndp.batch_sample(batch_size=1000)
+    cv_generator = ndp.batch_sample(batch_size=10000,work_type='cv')
     for i in range(100): m.train(train_generator=train_generator,cv_generator=cv_generator)
+    '''
